1804ff7a7c65e11af56cd50e6b8326ac759e5e38
saddamdeveloper <sishack8@gmail.com>
2019-06-28 14:32:30 +0530

    Today's Commit

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,22 +1,35 @@
 <?php require_once '../app/views/inc/header.php' ?>
-<div class="row">
+<div class="row mb-4">
     <div class="col-md-8">
         <div class="flower-2"></div>
     </div>
     <div class="col-md-4">
-        <div class="signup-cover">
+        <div class="login-cover">
             <div class="card">
                 <div class="card-header">
                     <div class="form-heading">
                         <div class="row">
                             <div class="col-md-7">
-                                <div class="register-logo"></div>
+                                <div class="login-logo"></div>
                             </div>
                             <div class="col-md-5">
                                 <div class="flower text-right"></div>
                             </div>
                         </div>
                     </div>
+                </div>
+                <div class="card-body">
+                    <form id="form-submit" method="post">
+                        <div class="form-group">
+                            <input type="text" id="fname" class="form-control" name="fname" placeholder="আপোনাৰ ইমেইল" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" id="fname" class="form-control" name="fname" placeholder="আপোনাৰ পাচৱৰ্ড" autocomplete="off">
+                        </div>
+                        <div class="form-group">
+                            <button class="button" style="vertical-align:middle"><span>লগিন কৰক</span>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
